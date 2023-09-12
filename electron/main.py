7535458a0ba8b64abb8d4b2fd6432d959a7d508e
MhdTarhini <mohamedtarhini95@gmail.com>
2023-09-12 09:31:01 +0300

    Frontend <> Create a Python Virtual Environment & Init ezdxf

--- electron/main.py
+++ electron/main.py
@@ -1,10 +1,34 @@
 import sys
+import ezdxf
 
-def process_data(product_data):
-    print("Received data from Electron:", product_data)
+def extractData(data):
+    doc = ezdxf.readfile("typical-model.dxf")
+
+    msp = doc.modelspace()
+
+    with open("results.txt", "w") as f:
+        for entity in msp:
+            etype = entity.dxftype()
+            f.write(f"Entity Type: {etype}\n")
+        
+            if etype == "LINE":
+                f.write(f"Start: {entity.dxf.start}, End: {entity.dxf.end}, layer:{entity.dxf.layer}\n")
+            elif etype == "CIRCLE":
+                f.write(f"Center: {entity.dxf.center}, Radius: {entity.dxf.radius}\n")
+            elif etype == "ARC":
+                f.write(f"Center: {entity.dxf.center}, Radius: {entity.dxf.radius}, Start Angle: {entity.dxf.start_angle}, End Angle: {entity.dxf.end_angle}\n")
+            elif etype == "MTEXT":
+                f.write(f"Insertion Point: {entity.dxf.insert}, Text: {entity.dxf.text}\n")
+            elif etype == "LWPOLYLINE":
+                for Vertex in entity:
+                    x, y = Vertex[0], Vertex[1]
+                    f.write ((f"x:{x} y:{y} layer:{entity.dxf.layer}\n"))
+          
+                    f.write(f"Is Closed: {'Yes' if entity.is_closed else 'No'}\n")
+    print("is done")
 
 # Read data sent from Electron
 if __name__ == "__main__":
     for line in sys.stdin:
         data = line.strip()
-        process_data(data)
+        extractData(data)
