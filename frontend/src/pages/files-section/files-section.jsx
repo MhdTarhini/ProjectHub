ce1e0412cebcc85143f9e95466fe330d3c36025e
MhdTarhini <mohamedtarhini95@gmail.com>
2023-09-15 19:41:51 +0300

    Frontend <> Impliment Modal to Handle Uploaded File

--- frontend/src/pages/files-section/files-section.jsx
+++ frontend/src/pages/files-section/files-section.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { channels } from "../../shared/constants";
 import "./files-section.css";
+import Modal from "react-modal";
+import Input from "../../component/input/input";
 
 function FilesSection() {
   const [file, setfile] = useState([]);
   const [update, setUpdate] = useState([]);
+  const [fileName, setFileName] = useState("");
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const [compareSuccess, setCompareSuccess] = useState(false);
   const [CompareResult, setCompareResult] = useState("");
@@ -12,10 +15,19 @@ function FilesSection() {
   const [detailsSuccess, setDetailsSuccess] = useState(false);
   const [getSvg, setGetSvg] = useState("");
   const [SvgSuccess, setSvgSuccess] = useState(false);
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-  function handleUpload(e) {
-    setfile(e.target.files[0]);
-    const file_uploaded = e.target.files[0];
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleUpload() {
+    const file_uploaded = file;
     if (file_uploaded) {
       const reader = new FileReader();
       reader.onload = (event) => {
@@ -54,6 +66,10 @@ function FilesSection() {
     }
   }
 
+  function handleFileName(e) {
+    console.log(e.target.value);
+  }
+
   useEffect(() => {
     window.electron.on(channels.Extract_Data_IsDone, (data) => {
       setUploadSuccess(true);
@@ -74,58 +90,91 @@ function FilesSection() {
   }, []);
 
   return (
-    <div className="files-section">
-      <div className="top-file-section">
-        <div className="file-section-title">Files Section</div>
-        <label className="new-file" htmlFor="new-file">
-          New file
-        </label>
-        <input
-          type="file"
-          id="new-file"
-          name="new-file"
-          onChange={(e) => {
-            handleUpload(e);
-          }}
-          className="none"
-        />
-      </div>
-      <div className="hr"></div>
-      <div className="branches-filter">
-        <div>branches</div>
-        <div>Fileter</div>
-      </div>
-      <div className="hr"></div>
-      <div className="file-section-card">
-        <div className="card-container">
-          <div className="card">
-            <img
-              src="./hello.svg"
-              className="file-section-card-img"
-              alt="Description"
-            />
-            <div className="middle-card">
-              <div className="file-name">hello world</div>
-              <div className="card-option">
-                <div className="point"></div>
-                <div className="point"></div>
-                <div className="point"></div>
-              </div>
-            </div>
-            <div className="uploaded-by">User name</div>
+    <>
+      <div className="files-section">
+        <div className="top-file-section">
+          <div className="file-section-title">Files Section</div>
+          <div onClick={openModal} className="btn">
+            New File
           </div>
         </div>
-      </div>
+        <div className="hr"></div>
+        <div className="branches-filter">
+          <div>branches</div>
+          <div>Fileter</div>
+        </div>
+        <div className="hr"></div>
+        <div className="file-section-card">
+          <div className="card-container">
+            <div className="card">
+              <img
+                src="./hello.svg"
+                className="file-section-card-img"
+                alt="Description"
+              />
+              <div className="middle-card">
+                <div className="file-name">hello world</div>
+                <div className="card-option">
+                  <div className="point"></div>
+                  <div className="point"></div>
+                  <div className="point"></div>
+                </div>
+              </div>
+              <div className="uploaded-by">User name</div>
+            </div>
+          </div>
+        </div>
 
-      <button onClick={handleUpdate}> update file</button>
-      <input
-        type="file"
-        name="update"
-        onChange={(e) => {
-          setUpdate(e.target.files[0]);
-        }}
-      />
-    </div>
+        <button onClick={handleUpdate}> update file</button>
+        <input
+          type="file"
+          name="update"
+          onChange={(e) => {
+            setUpdate(e.target.files[0]);
+          }}
+        />
+      </div>
+      <div className="model-container">
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          ariaHideApp={false}
+          className="new-file-model">
+          <h2 className="model-title">Upload New File</h2>
+          <form className="upload-file-form">
+            <div className="file-name-conatainer">
+              <Input
+                label={"File Name"}
+                name={"file-name"}
+                type={"text"}
+                onchange={handleFileName}
+              />
+            </div>
+            <div className="input-upload-file">
+              <label className="btn" htmlFor="new-file">
+                upload
+              </label>
+              <input
+                type="file"
+                id="new-file"
+                name="new-file"
+                onChange={(e) => {
+                  setfile(e.target.files[0]);
+                }}
+                className="none"
+              />
+              <div>{file.name}</div>
+            </div>
+            <div className="btns">
+              <button className="btn">submit</button>
+              <button className="btn" onClick={closeModal}>
+                close
+              </button>
+            </div>
+          </form>
+        </Modal>
+      </div>
+    </>
   );
 }
 
