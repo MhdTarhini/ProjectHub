8286f09300dff73eb03da5faf862cb7ff05c3b11
MhdTarhini <mohamedtarhini95@gmail.com>
2023-09-24 11:08:53 +0300

    Frontend <> Init Firebase & React-firebase-hooks

---
+++ frontend/src/component/chatComponent/sidebar/ChatSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatNavbar from "../navbar/ChatNavbar";
+
+function ChatSidebar() {
+  return (
+    <div className="chat-sidebar">
+      <ChatNavbar />
+    </div>
+  );
+}
+
+export default ChatSidebar;
--- frontend/src/pages/chats-section/chatsSection.jsx
+++ frontend/src/pages/chats-section/chatsSection.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import ChatSidebar from "../../component/chatComponent/sidebar/ChatSidebar";
+import Chat from "../../component/chatComponent/chat/Chat";
+import "./chatsSection.css";
 
 function ChatsSection() {
-  return <div>ChatsSection</div>;
+  return (
+    <div className="chat-section">
+      <div className="chat-container">
+        <ChatSidebar />
+        <Chat />
+      </div>
+    </div>
+  );
 }
 
 export default ChatsSection;
